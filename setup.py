e3bf8c3e3a2b5a4afb992c6872318828969644db
Cezar Sas <cezar.sas@gmail.com>
2020-07-30 17:29:05 +0200

    Fix to travis deploy versions

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/SasCezar/code-cleaner',
-    version='0.1.2',
+    version='0.1.2-dev',
     zip_safe=False,
 )
